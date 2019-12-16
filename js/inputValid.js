632711938d1014d4c97f8bd1c448de12fba2ce2d
kevinguggermaier <kevin.guggermaier@edu.fh-joanneum.at>
2019-12-16 15:04:27 +0100

    added valid for number

--- js/inputValid.js
+++ js/inputValid.js
@@ -1,10 +1,13 @@
-function not_a_Number(value) {
+function checkNumber(value) {
     if (!value.match(/^[\d]*$/)) {
-        
-        return true;
-    } else {
-        return false;
+        value = value.slice(0, value.length-1);
+        if(getNumberOfInvalidChar(value) >= 1){
+            value = null;
+            return null;
+        }else
+            return value;
     }
+    return value;
 }
 
 function getNumberOfInvalidChar(value){
@@ -14,59 +17,71 @@ function getNumberOfInvalidChar(value){
     else
         return 0;
 }
-/*function shortCut() {
-}
 
-function roomType() {
-    
-}*/
+function checkLength(value, max) {
+    if(value.length > max){
+        return value.slice(0, max);
+    }
+    return value;
+}
 
 function roomNumber() {
     let value = document.getElementById("RoomNumber").value;
-    if(not_a_Number(value)){
-        console.log("not a number");
-        value = value.slice(0, value.length-1);
-        if(getNumberOfInvalidChar(value) >= 1){
-            value = null;
-        }
-        document.getElementById("RoomNumber").value = value;
-    }
+    value = checkNumber(value);
+    value = checkLength(value, 3);
+    document.getElementById("RoomNumber").value = value;
+}
+
+
+function floor() {
+    let value = document.getElementById("Floor").value;
+    value = checkNumber(value);
+    value = checkLength(value, 1);
+    document.getElementById("Floor").value = value;
+}
+
+
+function shortCut() {
+    let value = document.getElementById("ShortCut").value;
 }
 
 /*
-function floor() {
+function roomType() {
+    let value = document.getElementById("RoomType").value;
     
 }
 
 function location() {
-    
+    let value = document.getElementById("Location").value;
 }
 
 function locationStreet() {
-    
+    let value = document.getElementById("LocationStreet").value;
 }
 
 function locationPlz() {
-    
+    let value = document.getElementById("LocationPlz").value;
 }
 
 function maDesc() {
-    
+    let value = document.getElementById("MA_Desc").value;
 }
 
 
 function maDate(){
-    
+    let value = document.getElementById("MA_Date").value;
 }
 
 function maRemark(){
-}*/
+    let value = document.getElementById("MA_Remark").value;
+}
 
-/*document.getElementById("ShortCut").addEventListener("keyup", shortCut);
+
+document.getElementById("ShortCut").addEventListener("keyup", shortCut);
 document.getElementById("RoomType").addEventListener("keyup", roomType);*/
 document.getElementById("RoomNumber").addEventListener("keyup", roomNumber);
-/*document.getElementById("Floor").addEventListener("keyup", floor);
-document.getElementById("Location").addEventListener("keyup", location);
+document.getElementById("Floor").addEventListener("keyup", floor);
+/*document.getElementById("Location").addEventListener("keyup", location);
 document.getElementById("LocationStreet").addEventListener("keyup", locationStreet);
 document.getElementById("LocationPlz").addEventListener("keyup", locationPlz);
 document.getElementById("MA_Desc").addEventListener("keyup", maDesc);
