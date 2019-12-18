90572803dff5c3ed36aaeb6f99243b3132a76f1d
kevinguggermaier <kevin.guggermaier@edu.fh-joanneum.at>
2019-12-18 19:25:42 +0100

    added input valid

--- js/inputValid.js
+++ js/inputValid.js
@@ -1,9 +1,12 @@
 let callAlert = false;
 
-function checkNumber(value) {
+function checkNumber(value, setCallAlert) {
+
     if (!value.match(/^[\d]*$/)) {
         value = value.slice(0, value.length-1);
-        callAlert = true;
+        if(setCallAlert){
+            callAlert = true;
+        }
         if(getNumberOfInvalidChar(value) >= 1){
             alert("Zu viele Invalide Zeichen\nEingabe wird zurückgesetzt");
             value = null;
@@ -17,7 +20,6 @@ function checkNumber(value) {
 }
 
 function getNumberOfInvalidChar(value){
-    console.log("value" , value);
     let t = value.match(/(\D)/);
     if(t != null)
         return t.length;
@@ -25,21 +27,34 @@ function getNumberOfInvalidChar(value){
         return 0;
 }
 
-function checkLength(value, max) {
+function checkLength(value, max, setCallAlert) {
     if(value === null)
         return value;
 
     if(value.length > max){
+
+        alert("Maximale Eingabe erreicht \nMaximal " + max + " Zeichen erlaubt");
+        if(setCallAlert){
+            callAlert = true;
+        }
+
         return value.slice(0, max);
     }
     return value;
 }
 
 function checkString(value) {
+    if (value.length === 0) {
+        return value;
+    }
+
     if(!value.match(/^[a-z,ö,ä,ü,A-Z,Ä,Ö,Ü, ]+$/)){
         value = value.slice(0, value.length-1);
         if(!value.match(/^[a-z,ö,ä,ü,A-Z,Ä,Ö,Ü, ]+$/)){
+            alert("Unerlaubte Eingabe \nNur Buchstaben erlaubt");
             return  null;
+        }else {
+            alert("Unerlaubtes Zeichen \nNur Buchstaben erlaubt");
         }
     }
     return value;
@@ -47,16 +62,16 @@ function checkString(value) {
 
 function roomNumber() {
     let value = document.getElementById("RoomNumber").value;
-    value = checkNumber(value);
-    value = checkLength(value, 3);
+    value = checkNumber(value, true);
+    value = checkLength(value, 3, true);
     document.getElementById("RoomNumber").value = value;
 }
 
 
 function floor() {
     let value = document.getElementById("Floor").value;
-    value = checkNumber(value);
-    value = checkLength(value, 1);
+    value = checkNumber(value, false);
+    value = checkLength(value, 1, false);
     document.getElementById("Floor").value = value;
 }
 
@@ -70,30 +85,39 @@ function shortCut() {
         case 1:
             if(!value.match(/[a-z,A-Z]/)){
                 value = null;
+                alert("Unerlaubtes Zeichen \nNur Buchstaben erlaubt");
+                callAlert = true;
             }
-            console.log("1", value);
             break;
         case 2:
         case 3:
         case 4:
             if(!value.match(/([a-z,A-Z]{1}[0-9]+)$/)){
                 value = value.substr(0, value.length-1)
+                alert("Unerlaubtes Zeichen \nNur Ziffern erlaubt");
+                callAlert = true;
             }
             value = value.replace(/(([a-z,A-Z]){1})/, "$1.");
-            console.log("4", value);
             break;
         default:
             if(!value.match(/([a-z,A-Z]{1}[0-9]+)$/)){
                 value = value.slice(0, value.length-1)
+                alert("Unerlaubtes Zeichen \nNur Ziffern erlaubt");
+                callAlert = true;
             }
-            value = value.slice(0,5);
-            if(!value.match(/([a-z,A-Z]{1})([0-9]{3})([0-9]{1})/)){
+            if(value.length == 6){
+                value = value.slice(0,5);
+                alert("Maximum erreicht \nzB.: G.100.2");
+                callAlert = true;
+            }
+            //
+            if(!value.match(/([a-z,A-Z]{1})([0-9]{3})([0-9]{1})$/) && value.length > 5){
                 value = null;
+                alert("Unerlaubter Eingabe \n Bsp.: G.100.2");
+                callAlert = true;
             }else{
                 value = value.replace(/([a-z,A-Z]{1})([0-9]{3})([0-9]{1})/, "$1.$2.$3");
-
             }
-             console.log("default");
             break;
     }
 
@@ -103,25 +127,19 @@ function shortCut() {
 
 
 function roomType() {
-    console.log("Raum type")
     let value = document.getElementById("RoomType").value;
-    console.log("value", value)
-    if(!value.match(/^[a-z,ö,ä,ü,A-Z,Ä,Ö,Ü, ]+$/)){
-        value = value.slice(0, value.length-1);
-        if(!value.match(/^[a-z,ö,ä,ü,A-Z,Ä,Ö,Ü, ]+$/)){
-            value = null;
-        }
-    }
-    document.getElementById("RoomType").value = value;
+    document.getElementById("RoomType").value = checkString(value);
 }
 
 function locationPlz() {
     let value = document.getElementById("LocationPlz").value;
     if (value.length === 1 && value[0] === "0") {
         value = null;
+        alert("Postleitzahl kann nicht mit 0 beginnen");
+        callAlert = true;
     }else{
-        value = checkNumber(value);
-        value = checkLength(value, 4);
+        value = checkNumber(value, true);
+        value = checkLength(value, 4, true);
     }
     document.getElementById("LocationPlz").value = value;
 }
@@ -131,7 +149,7 @@ function maDate(){
 
     value = value.replace(/[-]/g, "");
 
-    value = checkNumber(value);
+    value = checkNumber(value, true);
 
     if(value === null){
         document.getElementById("MA_Date").value = value;
@@ -143,9 +161,13 @@ function maDate(){
             value = value.replace(/^(\d{4})/, "$1-");
             if(value.match(/^(\d{4}-)[2-9]/)){
                 value = value.replace(/^(\d{4}-)[2-9]/, "$1");
+                alert("Unerlaubte Zahl \nfür Monat ist nur ein Wert zwischen 01 und 12 gültig");
+                callAlert = true;
             }
             if(value.match(/^(\d{4}-)1[3-9]/)){
                 value = value.replace(/^(\d{4}-)1[3-9]/, "$1");
+                alert("Unerlaubte Zahl \nfür Monat ist nur ein Wert zwischen 01 und 12 gültig");
+                callAlert = true;
             }
             break;
         case 7:
@@ -153,14 +175,29 @@ function maDate(){
             value = value.replace(/^(\d{4})(\d{2})/, "$1-$2-");
             if(value.match(/^(\d{4}-)(\d{2}-)[4-9]/)){
                 value = value.replace(/^(\d{4}-)(\d{2}-)[4-9]/, "$1$2");
+                alert("Unerlaubte Zahl \nfür Tag ist nur ein Wert zwischen 01 und 31 gültig");
+                callAlert = true;
             }
             if(value.match(/^(\d{4}-)(\d{2}-)3[2-9]/)){
                 value = value.replace(/^(\d{4}-)(\d{2}-)3[2-9]/, "$1$2");
+                alert("Unerlaubte Zahl \nfür Tag ist nur ein Wert zwischen 01 und 31 gültig");
+                callAlert = true;
             }
             break;
         default:
-            value = value.substr(0, 8);
-            value = value.replace(/^(\d{4})(\d{2})(\d{2})/, "$1-$2-$3");
+            if(value.length > 9){
+                value = null;
+                alert("Unerlaubte Eingabe \nErlaubte Eingabe z.B.: 2019-12-31");
+                callAlert = true;
+            }else{
+                if(value.length === 9){
+                    alert("Maximale Eingabe erreicht");
+                    callAlert = true;
+                }
+                value = value.substr(0, 8);
+                value = value.replace(/^(\d{4})(\d{2})(\d{2})/, "$1-$2-$3");
+            }
+
             break;
     }
 
@@ -179,21 +216,34 @@ function maDesc() {
 
 function maRemark(){
     let value = document.getElementById("MA_Remark").value;
+    if(value.length === 0){
+        return;
+    }
     if(!value.match(/^[a-z,ö,ä,ü,A-Z,Ä,Ö,Ü, ,0-9]+$/)){
         value = value.slice(0, value.length-1);
         if(!value.match(/^[a-z,ö,ä,ü,A-Z,Ä,Ö,Ü, ,0-9]+$/)){
             value = null;
+            alert("Unerlaubte Eingabe \nNur Buchstaben und Ziffern erlaubt");
+        }else{
+            alert("Unerlaubtes Zeichen \nNur Buchstaben und Ziffern erlaubt");
         }
     }
-
+    document.getElementById("MA_Remark").value = value;
 }
 
 function locationStreet() {
     let value = document.getElementById("LocationStreet").value;
+    if(value.length === 0){
+        return;
+    }
     if(!value.match(/^[a-z,ö,ä,ü,A-Z,Ä,Ö,Ü, ,0-9]+$/)){
         value = value.slice(0, value.length-1);
         if(!value.match(/^[a-z,ö,ä,ü,A-Z,Ä,Ö,Ü, ,0-9]+$/)){
             value = null;
+            alert("Unerlaubte Eingabe \nNur Buchstaben und Ziffern erlaubt");
+        }else{
+
+            alert("Unerlaubtes Zeichen \nNur Buchstaben und Ziffern erlaubt");
         }
     }
     document.getElementById("LocationStreet").value = value;
@@ -207,6 +257,10 @@ function fOutShortCut(){
     }
     if (confirm("Geben Sie eine vollständige Kurzbezeichnung ein [zB.: G.100.1]\n Bei Abbrechen wird der Inhalt gelöscht")) {
         document.getElementById("ShortCut").focus();
+        document.getElementById("ShortCut").removeEventListener("focusout", fOutShortCut);
+        setTimeout(() => {
+            document.getElementById("ShortCut").addEventListener("focusout", fOutShortCut);
+        },50);
     } else {
         document.getElementById("ShortCut").value = null;
     }
@@ -220,6 +274,10 @@ function fOutMaDate(){
     }
     if (confirm("Geben Sie eine vollständiges Datum ein [zB.: 2019-11-11]\n Bei Abbrechen wird der Inhalt gelöscht")) {
         document.getElementById("MA_Date").focus();
+        document.getElementById("MA_Date").removeEventListener("focusout", fOutMaDate);
+        setTimeout(() => {
+            document.getElementById("MA_Date").addEventListener("focusout", fOutMaDate);
+        },50);
     } else {
         document.getElementById("MA_Date").value = null;
     }
@@ -234,6 +292,10 @@ function fOutRoomNumber(){
 
     if (confirm("Geben Sie eine vollständige Raumnummer ein [zB.: 001 oder 100]\n Bei Abbrechen wird der Inhalt gelöscht")) {
         document.getElementById("RoomNumber").focus();
+        document.getElementById("RoomNumber").removeEventListener("focusout", fOutRoomNumber);
+        setTimeout(() => {
+            document.getElementById("RoomNumber").addEventListener("focusout", fOutRoomNumber);
+        },50);
     } else {
         document.getElementById("RoomNumber").value = null;
     }
@@ -247,6 +309,10 @@ function fOutLocationPlz(){
     }
     if (confirm("Geben Sie eine vollständige Postleitzahl ein [zB.: 8020]\n Bei Abbrechen wird der Inhalt gelöscht")) {
         document.getElementById("LocationPlz").focus();
+        document.getElementById("LocationPlz").removeEventListener("focusout", fOutLocationPlz);
+        setTimeout(() => {
+            document.getElementById("LocationPlz").addEventListener("focusout", fOutLocationPlz);
+        },50);
     } else {
         document.getElementById("LocationPlz").value = null;
     }
